7fe31f36558d69fe9a7164ce1f39d555540c7d23
KairosMP <109192850+KairosMP@users.noreply.github.com>
2023-08-25 12:21:39 -0400

    alterações no form-multi-steps

--- form/src/components/changeText.js
+++ form/src/components/changeText.js
@@ -1,17 +1,37 @@
-function changeText(textChangeElement, textSmallChangeElement, newText, newSmallText) {
+function updateTextAndPlaceholder(
+  textChangeElement,
+  textSmallChangeElement,
+  newText,
+  newSmallText,
+  newPlaceholder,
+) {
   textChangeElement.textContent = newText;
   textSmallChangeElement.textContent = newSmallText;
+  newPlaceholder && (semesterInput.placeholder = newPlaceholder);
 }
 
 const changeTextYes = document.querySelector('#field_visible_student');
 const changeTextNot = document.querySelector('#field_hidden_student');
 const textChange = document.querySelector('#text_change');
 const textSmallChange = document.querySelector('#small_text_change');
+const semesterInput = document.querySelector('#semester');
 
 changeTextYes.addEventListener('click', () => {
-  changeText(textChange, textSmallChange, "Onde Estuda", "Esse campo se refere ao ano que está.");
+  updateTextAndPlaceholder(
+    textChange,
+    textSmallChange,
+    'Onde estuda',
+    'Esse campo se refere ao ano que está.',
+    'Ex: 8º Ano',
+  );
 });
 
 changeTextNot.addEventListener('click', () => {
-  changeText(textChange, textSmallChange, "Onde Estudou", "Esse campo se refere ao ano em que terminou.");
+  updateTextAndPlaceholder(
+    textChange,
+    textSmallChange,
+    'Onde estudou',
+    'Esse campo se refere ao ano em que terminou.',
+    'Ex: 2010',
+  );
 });
--- form/src/components/maskInputs.js
+++ form/src/components/maskInputs.js
@@ -1,23 +1,39 @@
 const contactPrivate = new Cleave('.contact_private', {
-  delimiters: ['(',')', ' ','-'],
-  blocks: [0,2,0,5,4],
-  numericOnly: true
-})
+  delimiters: ['(', ')', ' ', '-'],
+  blocks: [0, 2, 0, 5, 4],
+  numericOnly: true,
+});
 
 const contactPublic = new Cleave('.contact_public', {
-  delimiters: ['(',')', ' ','-'],
-  blocks: [0,2,0,5,4],
-  numericOnly: true
-})
+  delimiters: ['(', ')', ' ', '-'],
+  blocks: [0, 2, 0, 5, 4],
+  numericOnly: true,
+});
 
 const postalCode = new Cleave('#postal_code', {
   delimiters: ['-'],
-  blocks:[5,3],
-  numericOnly: true
+  blocks: [5, 3],
+  numericOnly: true,
 });
 
 const physicalPerson = new Cleave('#physical_person', {
   delimiters: ['.', '.', '-'],
-  blocks: [3,3,3,2],
-  numericOnly: true
+  blocks: [3, 3, 3, 2],
+  numericOnly: true,
+});
+
+const generalRecord = new Cleave('#general_record', {
+  delimiters: ['-'],
+  blocks: [7, 1],
+  numericOnly: true,
+});
+
+const peopleLive = new Cleave('#people_live', {
+  blocks: [2],
+  numericOnly: true,
+});
+
+const peopleWork = new Cleave('#people_work', {
+  blocks: [2],
+  numericOnly: true,
 });
